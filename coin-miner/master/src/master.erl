8744c71d983a459374bff7294789f40dbce6de4c
Shreya Shah <shreyashah690@gmail.com>
2022-09-21 13:40:03 -0400

    Handled no. of strings input on master.

--- coin-miner/master/src/master.erl
+++ coin-miner/master/src/master.erl
@@ -1,16 +1,16 @@
 -module(master).
 -import('./../../worker/src/worker', [start/2]).
--export([start/3, connect/1]).
--export([mining_result_manager/1, master_orchestrator/3]).
+-export([start/4, connect/2]).
+-export([mining_result_manager/1, master_orchestrator/4]).
 
 
-master_orchestrator(N, Server_name, Node_name) ->
+master_orchestrator(K, N, Server_name, Node_name) ->
   receive
     start ->
       register(mining_result_manager_proc, spawn(master, mining_result_manager, [[]])),
       register(master_connect, spawn(master, connect, [N])),
       worker:start(Server_name, Node_name),
-      master_orchestrator(N, Server_name, Node_name);
+      master_orchestrator(K, N, Server_name, Node_name);
     terminate ->
       mining_result_manager_proc ! terminate,
       mining_process_manager_proc ! terminate,
@@ -33,20 +33,28 @@ mining_result_manager(Curr_result) ->
       io:fwrite("Result manager termination - success.~n")
   end.
 
+assignWork(N) ->
+  MAX_WORK = 5,
+  if
+    N < MAX_WORK ->
+      N;
+    true ->
+      MAX_WORK
+  end.
 
-connect(N) ->
+connect(K, N) ->
   io:fwrite("Master is running~n."),
   receive
     {workerReady, Worker_id} ->
       io:fwrite("Sending signal to start worker with id ~p.~n", [Worker_id]),
-      Worker_id ! {start, N},
-      % TODO: Maintain Workers list
-    connect(N);
+      Work = assignWork(N),
+      Worker_id ! {start, K, Work},
+      connect(K, N-Work);
     terminate ->
       io:fwrite("Terminating master connect process")
   end.
 
 
-start(N, Server_name, Node_name) ->
-  register(master_orchestrator_proc, spawn(master, master_orchestrator, [N, Server_name, Node_name])),
+start(K, N, Server_name, Node_name) ->
+  register(master_orchestrator_proc, spawn(master, master_orchestrator, [K, N, Server_name, Node_name])),
   master_orchestrator_proc ! start.
